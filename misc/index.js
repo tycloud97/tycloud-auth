0951aad24e5c668026cb21d16f1c832d80b0106e
Ty <tycloud97@gmail.com>
2025-05-27 23:18:19 +0700

    đ

--- misc/index.js
+++ misc/index.js
@@ -3,83 +3,98 @@ const { CognitoUserPool, CognitoUser, AuthenticationDetails } = require('amazon-
 
 // Configuration for Cognito User Pool
 const poolData = {
-  UserPoolId: 'ap-southeast-1_F4feest0w', // Your user pool id here
-  ClientId: '21l1hhaf4lo8tskt87bjg8v40o'   // Your client id here
+    UserPoolId: 'ap-southeast-1_F4feest0w',
+    ClientId: '21l1hhaf4lo8tskt87bjg8v40o'
 };
 
 const userPool = new CognitoUserPool(poolData);
 
-// Function to authenticate with Cognito and retrieve tokens
-function authenticateUser(username, password) {
-  const authenticationDetails = new AuthenticationDetails({
-    Username: username,
-    Password: password,
-  });
+// Promisify authenticateUser
+function authenticateUserAsync(username, password) {
+    return new Promise((resolve, reject) => {
+        const authenticationDetails = new AuthenticationDetails({
+            Username: username,
+            Password: password,
+        });
 
-  const userData = {
-    Username: username,
-    Pool: userPool,
-  };
+        const userData = {
+            Username: username,
+            Pool: userPool,
+        };
 
-  const cognitoUser = new CognitoUser(userData);
+        const cognitoUser = new CognitoUser(userData);
 
-  cognitoUser.authenticateUser(authenticationDetails, {
-    onSuccess: (session) => {
-      const idToken = session.getIdToken().getJwtToken();
-      const accessToken = session.getAccessToken().getJwtToken();
-      console.log('ID Token:', idToken);
-      console.log('Access Token:', accessToken);
-      
-      // Use the ID token to get temporary AWS credentials
-      getAWSCredentials(idToken, () => {
-        // Once we have valid AWS credentials, list S3 buckets
-        listS3Buckets();
-      });
-    },
-    onFailure: (err) => {
-      console.error('Authentication failed:', err.message || JSON.stringify(err));
-    },
-  });
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: (session) => {
+                resolve(session);
+            },
+            onFailure: (err) => {
+                reject(err);
+            },
+        });
+    });
 }
 
-// Function to obtain AWS credentials using the ID token
-function getAWSCredentials(idToken, callback) {
-  const logins = {};
-  logins[`cognito-idp.ap-southeast-1.amazonaws.com/${poolData.UserPoolId}`] = idToken;
+// Promisify AWS credentials refresh
+function getAWSCredentialsAsync(idToken) {
+    return new Promise((resolve, reject) => {
+        const logins = {};
+        logins[`cognito-idp.ap-southeast-1.amazonaws.com/${poolData.UserPoolId}`] = idToken;
 
-  AWS.config.region = 'ap-southeast-1';
+        AWS.config.region = 'ap-southeast-1';
 
-  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'ap-southeast-1:4b14f089-5b51-43dc-ae8e-d4774d9b7259',
-    Logins: logins,
-  });
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'ap-southeast-1:4b14f089-5b51-43dc-ae8e-d4774d9b7259',
+            Logins: logins,
+        });
 
-  AWS.config.credentials.refresh((error) => {
-    if (error) {
-      console.error('Failed to refresh credentials:', error);
-    } else {
-      console.log('AWS credentials refreshed.');
-      callback();
+        AWS.config.credentials.refresh((error) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+// Promisify S3 listObjectsV2
+function listS3BucketsAsync() {
+    return new Promise((resolve, reject) => {
+        const s3 = new AWS.S3();
+        const params = {
+            Bucket: 'tycloud',
+        };
+
+        s3.listObjectsV2(params, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data.Contents);
+            }
+        });
+    });
+}
+
+// Main async function
+async function main(username, password) {
+    try {
+        const session = await authenticateUserAsync(username, password);
+        const idToken = session.getIdToken().getJwtToken();
+        const accessToken = session.getAccessToken().getJwtToken();
+        console.log('ID Token:', idToken);
+        console.log('Access Token:', accessToken);
+
+        await getAWSCredentialsAsync(idToken);
+        console.log('AWS credentials refreshed.');
+
+        const objects = await listS3BucketsAsync();
+        console.log('S3 Objects:');
+        objects.forEach(obj => console.log(obj.Key));
+    } catch (err) {
+        console.error('Error:', err.message || JSON.stringify(err));
     }
-  });
 }
 
-// Function to list objects in an S3 bucket
-function listS3Buckets() {
-  const s3 = new AWS.S3();
-  const params = {
-    Bucket: 'tycloud', // Your S3 bucket name
-  };
-
-  s3.listObjectsV2(params, (err, data) => {
-    if (err) {
-      console.error('Error listing S3 objects:', err);
-    } else {
-      console.log('S3 Objects:');
-      data.Contents.forEach(obj => console.log(obj.Key));
-    }
-  });
-}
-
-// Call this function with the username and password of a Cognito user
-// authenticateUser('typrone1@gmail.com', '');
+// Call main with username and password
+// main('typrone1@gmail.com', '');
